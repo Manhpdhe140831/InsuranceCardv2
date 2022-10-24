c4f42fb742de4f84fffa29ba86ebf99530825a53
manhpdhe140831 <manhpdhe140831@fpt.edu.vn>
2022-10-24 07:48:54 +0700

    source code

---
+++ src/main/java/com/swp/insurancecard/models/ERole.java
@@ -0,0 +1,7 @@
+package com.swp.insurancecard.models;
+
+public enum ERole {
+  ROLE_ADMIN,
+  ROLE_STAFF,
+  ROLE_CUSTOMER
+}
---
+++ src/main/java/com/swp/insurancecard/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.swp.insurancecard.repository;
+
+import java.util.Optional;
+
+import com.swp.insurancecard.models.ERole;
+import com.swp.insurancecard.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+  Optional<Role> findByName(ERole name);
+}
