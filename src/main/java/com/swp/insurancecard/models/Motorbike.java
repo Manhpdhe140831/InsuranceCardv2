ebc3e78efcad8f86defd9ac1e35788dabd4d763d
hoangtrung210400 <hoangtrung210400@gmail.com>
2022-10-26 09:55:48 +0700

    update entitites

--- src/main/java/com/swp/insurancecard/models/Motorbike.java
+++ src/main/java/com/swp/insurancecard/models/Motorbike.java
@@ -1,7 +1,46 @@
 package com.swp.insurancecard.models;
 
-public class Motorbike {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.*;
+import java.sql.Date;
+
+@Table(name = "tbl_motoBike")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Motorbike {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "license")
+    private String license;
+    @Column(name = "model")
+    private String model;
+    @Column(name = "manufacturer")
+    private String manufacturer;
+    @Column(name = "color")
+    private String color;
+    @Column(name = "yearOfMake")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date MFG;
+    @Column(name = "frameNumber")
+    private String frameNumber;
+    @Column(name = "engineNumber")
+    private String engineNumber;
+
+    private  String image;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 
 }
 
