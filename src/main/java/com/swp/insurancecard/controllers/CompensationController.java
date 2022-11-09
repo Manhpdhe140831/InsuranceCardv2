ecc315a49b41365114f328588d4e43700fcbd5a5
hoangtrung210400 <hoangtrung210400@gmail.com>
2022-11-09 23:00:44 +0700

    Update CompensationController.java

--- src/main/java/com/swp/insurancecard/controllers/CompensationController.java
+++ src/main/java/com/swp/insurancecard/controllers/CompensationController.java
@@ -1,13 +1,21 @@
 package com.swp.insurancecard.controllers;
 
+import com.swp.insurancecard.dto.AccidentDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/compensation")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class CompensationController {
 
+ 
 
 }
